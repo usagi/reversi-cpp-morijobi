6fda5a5fb654e9224576c099498c3c2e4a7533b3
usagi <usagi@WonderRabbitProject.net>
2011-09-29 13:50:59 +0900

    v0.0.1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <string>
 
+namespace cpp_morijobi{
+  namespace reversi{
+    class reversi{
+      void command_echo(const std::string& v) const{
+        std::cout << v << std::endl;
+      }
+      void get_command_line(std::string& command_line) const{
+        std::cout << "> ";
+        std::getline(std::cin, command_line);
+      }
+    public:
+      void run(){
+        for(std::string command_line; command_line != "exit"; get_command_line(command_line))
+          if(command_line.find("echo ") == 0)
+            command_echo(command_line.substr(5));
+      }
+    };
+  }
+}
+
 main(){
-
-  for(std::string input_command_line = ""; input_command_line != "exit"; std::cin >> input_command_line)
-    std::cout << input_command_line;
-
+  cpp_morijobi::reversi::reversi().run();
 }
 
